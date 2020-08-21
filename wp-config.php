b6675072a1a70685a8873f80a31ba3966d7b0641
Ali Muhammad <AliMuhammadOfficial>
2020-08-21 12:33:20 +0500

    config file updated

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress' );
+define( 'DB_NAME', 'sql9361584' );
 
 /** MySQL database username */
-define( 'DB_USER', 'wcapiuser' );
+define( 'DB_USER', 'sql9361584' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'wcapipass' );
+define( 'DB_PASSWORD', 'Jl1T7CSw5i' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'sql9.freesqldatabase.com' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
